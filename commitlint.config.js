93c8b9ac448e49eaf9b66932b1f6ce7fce2b4d09
Tamir Azrab <tamirazrab@gmail.com>
2025-12-02 09:00:00 +0500

    chore: initialize workspace tooling

---
+++ commitlint.config.js
@@ -0,0 +1,38 @@
+const { readdirSync } = require("node:fs");
+
+const getDirectories = (source) =>
+	readdirSync(source, { withFileTypes: true })
+		.filter((dirent) => dirent.isDirectory())
+		.map((dirent) => dirent.name);
+
+const scopes = [];
+for (const path of getDirectories("./src").map((p) => `./src/${p}`)) {
+	const files = readdirSync(path, { withFileTypes: true });
+	scopes.push(
+		...files.filter((item) => item.isDirectory()).map((item) => item.name),
+	);
+}
+
+scopes.push(
+	"remove",
+	"revert",
+	"conflict",
+	"config",
+	"entity",
+	"utils",
+	"deps",
+	"modules",
+	"test",
+	"migration",
+	"core",
+	"swagger",
+);
+
+module.exports = {
+	extends: ["@commitlint/config-conventional"],
+	ignores: [(message) => message.includes("release")],
+	rules: {
+		"scope-empty": [2, "never"],
+		"scope-enum": [2, "always", scopes],
+	},
+};
