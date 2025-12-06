c12f85eb52ce2555097ae8bf28d6bdd3505cd026
Tamir Azrab <tamirazrab@gmail.com>
2025-12-06 23:00:00 +0500

    chore: configure backend compiler modules

---
+++ apps/backend/src/infra/module.ts
@@ -0,0 +1,26 @@
+import { Module } from "@nestjs/common";
+
+import { MemoryCacheModule } from "./cache/memory";
+import { RedisCacheModule } from "./cache/redis";
+import { MongoDatabaseModule } from "./database/mongo";
+import { PostgresDatabaseModule } from "./database/postgres/module";
+import { EmailModule } from "./email";
+import { ExecutorInfraModule } from "./executor/module";
+import { HttpModule } from "./http";
+import { LoggerModule } from "./logger";
+import { SecretsModule } from "./secrets";
+
+@Module({
+	imports: [
+		SecretsModule,
+		MongoDatabaseModule,
+		PostgresDatabaseModule,
+		LoggerModule,
+		HttpModule,
+		RedisCacheModule,
+		MemoryCacheModule,
+		EmailModule,
+		ExecutorInfraModule,
+	],
+})
+export class InfraModule {}
